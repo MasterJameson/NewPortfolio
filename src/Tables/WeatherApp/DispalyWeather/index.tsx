ee298d8ecae4ea468593a347687f5cf87ef18b2d
Master <bagainjameson@gmail.com>
2022-02-20 16:48:14 +0800

    added table for weather

--- src/Tables/WeatherApp/DispalyWeather/index.tsx
+++ src/Tables/WeatherApp/DispalyWeather/index.tsx
@@ -1,10 +1,62 @@
 import React from 'react'
 
+
 const WeatherTable = (props: any) => {
-  console.log(props.props);
-  
+
+  const value = props.props
+
+  const temp = value?.main?.temp
+  const subTemp = (temp ? (temp - 212) : 0)
+  const celsius = (Math.floor((subTemp - 32) * 5 / 9))
+  const cloudiness = value?.weather?.map((val: any) => val.main)
+
+  console.log(temp);
+  console.log(value);
+
   return (
-    <div>WeatherTable</div>
+    <>
+      <h2>Weather Forecast</h2>
+      <table className='table  table-striped table-hover'>
+        <tbody>
+          <tr>
+            <td>City name</td>
+            <td>{value.name}</td>
+          </tr>
+          <tr>
+            <td>Cloudiness</td>
+            <td>{cloudiness}</td>
+          </tr>
+          <tr>
+            <td>Pressure</td>
+            <td>{value?.main?.pressure}</td>
+          </tr>
+          <tr>
+            <td>Humidity</td>
+            <td>{value?.main?.feels_like}</td>
+          </tr>
+          <tr>
+            <td>Temperature</td>
+            <td>{value?.main?.temp}</td>
+          </tr>
+          <tr>
+            <td>Sunrise</td>
+            <td>{value?.sys?.sunrise}</td>
+          </tr>
+          <tr>
+            <td>Sunset</td>
+            <td>{value?.sys?.sunset}</td>
+          </tr>
+          <tr>
+            <td>Latitude</td>
+            <td>{value?.coord?.lat}</td>
+          </tr>
+          <tr>
+            <td>Longtitude</td>
+            <td>{value?.coord?.lon}</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   )
 }
 
