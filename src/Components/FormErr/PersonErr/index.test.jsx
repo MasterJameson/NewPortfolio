7ac7a42bcf1320cb6600ac4216a0d703089eb3b6
Master <bagainjameson@gmail.com>
2022-03-02 20:50:00 +0800

    added configure enzyme and test render

--- src/Components/FormErr/PersonErr/index.test.jsx
+++ src/Components/FormErr/PersonErr/index.test.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import PersonErr from '.'
+import { shallow } from 'enzyme'
+import PersonErr from './index'
 
-Enzyme.configure({ adapter: new Adapter() })
 
-describe('PersonErr', () => {
 
-  it("accepts products props", () => {
-    const prps = { age: "", email: "Email already exist.", fName: "", gender: "", lName: "", mobile: "Mobile already exist." }
-    expect(Object.keys(prps).sort()).toEqual(['email', 'mobile', 'gender', 'lName', 'fName', 'age'].sort())
-  });
+describe('Behavior and Render Test', () => {
+
+  let wrapper
+  const prps = { age: "", email: "Email already exist.", fName: "", gender: "", lName: "", mobile: "Mobile already exist." }
+
+  beforeEach(() => {
+    wrapper = shallow(<PersonErr {...prps} />)
+  })
+
+  it('Should render', () => {
+    expect(wrapper.length).toEqual(1)
+    console.log(wrapper.props())
+  })
+
+  // it("accepts products props", () => {
+  //   const prps = { age: "", email: "Email already exist.", fName: "", gender: "", lName: "", mobile: "Mobile already exist." }
+  //   expect(Object.keys(prps).sort()).toEqual(['email', 'mobile', 'gender', 'lName', 'fName', 'age'].sort())
+  // });
 
 })
--- src/Components/FormErr/PersonErr/index.tsx
+++ src/Components/FormErr/PersonErr/index.tsx
@@ -3,16 +3,18 @@ import React from 'react'
 export default function PersonErr(props: any): JSX.Element {
   const errorValue = props.isInputError;
 
+  const spanConditionRender = (fieldName: string, index: number) => {
+    if (errorValue[fieldName].length > 0) {
+      return (
+        <span key={index} className="m-0">{errorValue[fieldName]} </span>
+      )
+    }
+  }
+
   return (
     <React.Fragment>
       {props.length > 0 &&
-        Object.keys(props.isInputError).map((fieldName, i) => {
-          if (errorValue[fieldName].length > 0) {
-            return (
-              <span key={i} className="m-0">{errorValue[fieldName]} </span>
-            )
-          }
-        })
+        Object.keys(props.isInputError).map((fieldName, index) => spanConditionRender(fieldName, index))
       }
     </React.Fragment>
   )
