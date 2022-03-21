81df14377ca8e813e1a1cd31ed9bd3643b41ba95
Master <bagainjameson@gmail.com>
2022-03-21 13:52:49 +0800

    added https

--- src/redux/action.tsx
+++ src/redux/action.tsx
@@ -28,7 +28,7 @@ export const removePerson = (data: MyPerson) => {
 
 export const getWeatherApp = (data: string) => (dispatch: any) => {
   console.log(data)
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${data === '' ? 'manila' : data}&APPID=fde32f2b1f38982f8cf53ee3c5761528`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${data === '' ? 'manila' : data}&APPID=fde32f2b1f38982f8cf53ee3c5761528`;
   fetch(url,{method: "GET"}).then(response => response.json().then(data => {
     dispatch({
       type: 'GET_WEATHER',
@@ -38,7 +38,7 @@ export const getWeatherApp = (data: string) => (dispatch: any) => {
 }
 
 export const getMockApi = (data:string) =>(dispatch:any)=>{
-  const url = `http://localhost:3001/${data}`
+  const url = `https://localhost:3001/${data}`
   fetch(url,{method: "GET"}).then(response => response.json().then(data =>{
     dispatch({
       type: 'GET_PERSONLIST',
