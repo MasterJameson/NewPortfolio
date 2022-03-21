7757ca9ab156643c8efd01ab50ac46a0fd29e408
Master <bagainjameson@gmail.com>
2022-03-21 13:48:35 +0800

    added method in action for weatherapp

--- src/redux/action.tsx
+++ src/redux/action.tsx
@@ -29,7 +29,7 @@ export const removePerson = (data: MyPerson) => {
 export const getWeatherApp = (data: string) => (dispatch: any) => {
   console.log(data)
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${data === '' ? 'manila' : data}&APPID=fde32f2b1f38982f8cf53ee3c5761528`;
-  fetch(url).then(response => response.json().then(data => {
+  fetch(url,{method: "GET"}).then(response => response.json().then(data => {
     dispatch({
       type: 'GET_WEATHER',
       getApi: data
