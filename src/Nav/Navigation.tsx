230aa576edfcaf4a8bd337c1f024877a65f1a419
Master <bagainjameson@gmail.com>
2022-05-04 16:44:20 +0800

    added component in navigation

--- src/Nav/Navigation.tsx
+++ src/Nav/Navigation.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { makeStyles } from '@mui/styles';
 import { Link } from 'react-router-dom';
 import Box from '@mui/material/Box';
-
+import dropDownIcon from '../Assets/icons/dropDownIcon.png'
+import getActiveUser from '../Home/getActiveUse'
+import _ from 'lodash'
 
 const useStyles = makeStyles((theme: any) => ({
   openNavSection: {
@@ -41,23 +43,70 @@ const useStyles = makeStyles((theme: any) => ({
   },
   anchor: {
     display: 'block',
-    padding: '10px 30px',
+    padding: '10px 20px',
     color: '#fff',
     textDecoration: 'none',
     transition: '0.5s',
     [theme.breakpoints.down('sm')]: {
       textAlign: 'center',
     }
-
-  }
+  },
+  dropDownDiv: {
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    cursor: 'pointer',
+    padding: '10px 20px',
+  },
+  dropDownSpan: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dropDownStlye: {
+    paddingRight: 10,
+    color: '#fff',
+    fontSize: 16,
+    margin: 0,
+    '&:hover': {
+      color: '#0a58ca',
+    },
+  },
+  dropDownContent: {
+    position: 'absolute',
+    minWidth: 128,
+    zIndex: 1,
+    textAlign: 'center',
+    backgroundColor: '#fff',
+    border: '1px solid rgba(0,0,0,.15);',
+    borderRadius: '0.25rem',
+  },
+  dropDownAnchor: {
+    display: 'block',
+    padding: 5,
+    textDecoration: 'none',
+    transition: '0.5s',
+    color: '#16181b',
+    fontSize: 12,
+    '&:hover': {
+      backgroundColor: '#f8f9fa',
+    },
+    [theme.breakpoints.down('sm')]: {
+      textAlign: 'center',
+    }
+  },
 }))
 
 export const Nav = () => {
 
   const [showNav, setShowNav] = React.useState(false)
+  const [showComponents, setShowComponents] = React.useState(false);
 
   const classes = useStyles()
+  const user = getActiveUser()
 
+  console.log('user', user)
   const openNav = () => {
     setShowNav(showNav ? false : true)
   }
@@ -72,16 +121,24 @@ export const Nav = () => {
             <Link to={''} className={classes.anchor} onClick={openNav}>Home</Link>
           </li>
           <li>
-            <Link to={'add-person'} className={classes.anchor} onClick={openNav}>AddPerson</Link>
-          </li>
-          <li>
-            <Link to={'waether-app'} className={classes.anchor} onClick={openNav}>WeartherApp</Link>
+            {
+            }
+            <div className={classes.dropDownDiv} onClick={() => setShowComponents(showComponents ? false : true)}>
+              <p className={classes.dropDownStlye}>Components</p>
+              <span className={classes.dropDownSpan}><img src={dropDownIcon} alt="" /></span>
+            </div>
+            {showComponents &&
+              <div className={classes.dropDownContent} onMouseLeave={() => setShowComponents(false)}>
+                <Link to={'add-person'} className={classes.dropDownAnchor}>AddPerson</Link>
+                <Link to={'waether-app'} className={classes.dropDownAnchor}>WeartherApp</Link>
+              </div>
+            }
           </li>
           <li>
             <Link to={'login'} className={classes.anchor} onClick={openNav}>Login</Link>
           </li>
           <li>
-            <Link to={'sign-up'} className={classes.anchor} onClick={openNav}>Sign Up</Link>
+            <Link to={'sign-up'} className={classes.anchor} onClick={openNav}>Signup</Link>
           </li>
         </ul>
       </Box>
